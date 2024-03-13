6ef5caa315601cf7752fa8584c52d757eda9582e
Mohamed Fall <ps.hackmaster@gmail.com>
2024-03-13 10:17:03 +0000

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "static")));
+
+app.get("/", (_req, res) => {
+  res.sendFile(path.join(__dirname, "static", "index.html"));
+});
+
+app.listen(8080);
+console.log("app listening on port 8080");
